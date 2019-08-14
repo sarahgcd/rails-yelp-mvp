459f8df09b8a4d699a3d6dd0582ae00530db8172
Sarah Guichard <sarah.gcd1@gmail.com>
2019-08-14 17:17:33 +0200

    restaurant exercice

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,34 @@
+class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Bordeluche',
+    address:      '25 rue des Dames 75017 PARIS',
+    phone_number: '01 02 03 04 05',
+    category:     'french'
+  },
+  {
+    name:         'Dose',
+    address:      '82 Place du Dr Félix Lobligeois 75017 PARIS',
+    phone_number: '01 02 39 04 05',
+    category:     'french'
+  },
+  {
+    name:         'Fuxia',
+    address:      '17 rue Legendre 75017 PARIS',
+    phone_number: '01 02 03 04 07',
+    category:     'italian'
+  },
+  {
+    name:         'MME Shawn',
+    address:      '5 rue Legendre 75017 PARIS',
+    phone_number: '01 02 69 04 07',
+    category:     'chinese'
+  },
+  {
+    name:         'Aki',
+    address:      '17 rue Saint-Anne 75002 PARIS',
+    phone_number: '01 02 03 04 97',
+    category:     'japanese'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
